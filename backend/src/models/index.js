82c935aec19f06adfcc6ba25e079e34325073530
ifjika <jikapramuja221@gmail.com>
2025-09-10 20:27:17 +0700

    Fix merge with dev/tezza

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,23 +1,29 @@
-const sequelize = require('../../config/database');
-const User = require('./User');
-const Role = require('./Role');
-const Permission = require('./Permission');
-const UserRole = require('./UserRole');
-const RolePermission = require('./RolePermission');
-const Resource = require('./Resource');
-const RoomDetail = require('./RoomDetail');
-const HealthDetail = require('./HealthDetail');
-const VehicleDetail = require('./VehicleDetail');
-const CourseDetail = require('./CourseDetail');
-const Booking = require('./Booking');
-const AvailabilitySlot = require('./AvailabilitySlot');
+const sequelize = require("../../config/database");
+const User = require("./User");
+const Role = require("./Role");
+const Permission = require("./Permission");
+const UserRole = require("./UserRole");
+const RolePermission = require("./RolePermission");
+const Resource = require("./Resource");
+const RoomDetail = require("./RoomDetail");
+const HealthDetail = require("./HealthDetail");
+const VehicleDetail = require("./VehicleDetail");
+const CourseDetail = require("./CourseDetail");
+const Booking = require("./Booking");
+const AvailabilitySlot = require("./AvailabilitySlot");
 
 // Associations
-User.belongsToMany(Role, { through: UserRole, foreignKey: 'userId' });
-Role.belongsToMany(User, { through: UserRole, foreignKey: 'roleId' });
+User.belongsToMany(Role, { through: UserRole, foreignKey: "userId" });
+Role.belongsToMany(User, { through: UserRole, foreignKey: "roleId" });
 
-Role.belongsToMany(Permission, { through: RolePermission, foreignKey: 'roleId' });
-Permission.belongsToMany(Role, { through: RolePermission, foreignKey: 'permissionId' });
+Role.belongsToMany(Permission, {
+  through: RolePermission,
+  foreignKey: "roleId",
+});
+Permission.belongsToMany(Role, {
+  through: RolePermission,
+  foreignKey: "permissionId",
+});
 
 module.exports = {
   sequelize,
@@ -32,13 +38,13 @@ module.exports = {
   VehicleDetail,
   CourseDetail,
   Booking,
-  AvailabilitySlot
+  AvailabilitySlot,
 };
 
 // Sync function for migrations
 if (require.main === module) {
   sequelize.sync({ alter: true }).then(() => {
-    console.log('Database synced');
+    console.log("Database synced");
     process.exit();
   });
 }
